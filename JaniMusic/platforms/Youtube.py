807fe7c93a43f5ed6935eb20b389472ab21dca34
JaniPrivetRepo <fffdfhhbnjjh@gmail.com>
2026-03-23 01:45:41 +0530

    Update Youtube.py

--- JaniMusic/platforms/Youtube.py
+++ JaniMusic/platforms/Youtube.py
@@ -5,7 +5,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpython.future import VideosSearch
 from JaniMusic.utils.formatters import time_to_seconds
 import aiohttp
 from JaniMusic import LOGGER
